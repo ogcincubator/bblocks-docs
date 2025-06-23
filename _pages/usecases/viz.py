744029b6d29764134f555183c370735c7de251e4
Rob Atkinson <ratkinson@ogc.org>
2025-06-23 16:55:40 +1000

    Tweaked use case diagram test

--- _pages/usecases/viz.py
+++ _pages/usecases/viz.py
@@ -4,6 +4,7 @@ import graphviz
 dot = graphviz.Digraph(comment="OGC Building Block Use Case Relationships", format='png')
 
 # Use case nodes
+dot.node('U0', 'Publish FAIR Standards\n(Standard Editors,Standards Organisations)')
 dot.node('U1', '1. Reuse Requirements\n(Standard Editors)')
 dot.node('U2', '2. Schema Compatibility QC\n(Standards Devs)')
 dot.node('U3', '3. Example Validation\n(Tool Vendors)')
@@ -13,18 +14,28 @@ dot.node('U6', '6. Profiling Standards\n(Profile Designers)')
 dot.node('U7', '7. Infrastructure Design\n(Platform Providers)')
 dot.node('U8', '8. Application Design\n(App Developers)')
 dot.node('U9', '9. Discover Standards\n(New Users, AI)')
+dot.node('U01', 'Quality Control\n(Standards Organisations)')
 
 # Connections (simplified relationships)
 dot.edges([
+    ('U0', 'U1'),
+    ('U0', 'U6'),
+    ('U0', 'U01'),
+    ('U01', 'U2'),
     ('U9', 'U1'),
     ('U9', 'U6'),
+    ('U9', 'U7'),
     ('U1', 'U2'),
+    ('U01', 'U3'),
     ('U2', 'U3'),
-    ('U3', 'U4'),
-    ('U1', 'U5'),
+    ('U4', 'U3'),
+    ('U01', 'U4'),
+    ('U0', 'U5'),
     ('U6', 'U7'),
-    ('U6', 'U8'),
+    ('U6', 'U4'),
+    ('U8', 'U6'),
     ('U5', 'U8'),
+    ('U5', 'U3'),
     ('U9', 'U8')
 ])
 
